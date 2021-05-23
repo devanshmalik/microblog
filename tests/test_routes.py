777e7d408e14a6bbc72c40043bbf2365abf4a8bf
Devansh Malik <devanshmalik@hotmail.com>
2021-05-23 19:34:56 -0400

    Add login unit tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,24 +10,28 @@ def test_index_without_login(client):
         flash_message = dict(session['_flashes'])
         assert 'Please log in to access this page.' in flash_message['message']
 
-# def test_login_successful(client, auth):
-#     assert client.get('/login').status_code == 200
-#     response = auth.login(username='admin', password='password')
-#     assert response.headers['Location'] == 'http://localhost/index'
-#
-#     response = client.get('/index')
-#     assert b'Login requested for user admin, remember_me=False' in response.data
-#
-#
-# @pytest.mark.parametrize(
-#     ('username', 'password', 'message'),
-#     (
-#         ('', '', b'[This field is required.]'),
-#         ('test', '', b'[This field is required.]'),
-#         ('', 'test', b'[This field is required.]'),
-#     )
-# )
-# def test_login_validation(auth, username, password, message):
-#     response = auth.login(username, password)
-#     # print(response.data)
-#     assert message in response.data
+
+def test_login_successful(client, auth):
+    assert client.get('/login').status_code == 200
+    response = auth.login(username='dev', password='pass')
+    assert response.headers['Location'] == 'http://localhost/index'
+
+    response = client.get('/index')
+    assert response.status_code == 200
+    assert b'<title>Home - Microblog</title>' in response.data
+
+    response = client.get('/login')
+    assert response.headers['Location'] == 'http://localhost/index'
+
+
+@pytest.mark.parametrize(
+    ('username', 'password', 'message'),
+    (
+        ('', '', b'[This field is required.]'),
+        ('test', '', b'[This field is required.]'),
+        ('', 'test', b'[This field is required.]'),
+    )
+)
+def test_login_validation(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data

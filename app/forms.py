87df15fc8706def4d60bd6ebe3b595220b452691
Devansh Malik <devanshmalik@hotmail.com>
2021-05-26 20:15:18 -0400

    Add custom error pages

--- app/forms.py
+++ app/forms.py
@@ -36,3 +36,10 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(
+                f'Username already exists. Please use a different username.'
+            )
--- app/routes.py
+++ app/routes.py
@@ -98,6 +98,7 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
